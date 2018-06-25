b69c3bd90ff8043bbc8849a9379cc66c7a8e82b8
dvinubius <dvinubiu.narmon@gmail.com>
2018-06-25 08:37:26 +0300

    symmetry in home screen bg

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
 'use strict';
+var xoff = 0.00;
+let counterX;
+let counterXMax;
+let counterXDir = 1;
+let counterXStep = 1;
+
 
 function setup() {
 	createCanvas(windowWidth - 45, windowHeight);
 	// background(245,245,245);
   background(0,0,0);
 	colorMode(RGB, 255, 255, 255, 1);
-  // frameRate(10);
+  counterX = width/2;
+	counterXMax = width;
 }
 
 function draw() {
-  const green = random(0,255);
-  const posX = random(0,width);
+	if (counterX > counterXMax) {
+		counterXDir = -1;
+	}
+	if (counterX < 0) {
+		counterXDir = 1;
+	}
+	counterX += counterXDir * counterXStep;
+
+	const n1 = noiseVal(); // between 0 and 1
+	const n2 = noiseVal();
+  const green1 = map(n1,0,1,0,255);
+	const green2 = map(n2,0,1,0,255);
+	const alpha1 = map(n1,0,1,0.2,0.6);
+	const alpha2 = map(n2,0,1,0.2,0.6);
+	const posX1 = counterX;
+	const posX2 = width - counterX;
   const posY = random(0,height);
-	fill(0, green, 0, random(0,0.4));
   noStroke();
-	ellipse(posX, posY, 30,30);
+	fill(0, green1, 0, alpha1);
+	ellipse(posX1, posY, 30,30);
+	fill(0, green2, 0, alpha2);
+	ellipse(posX2, posY, 30,30);
 }
 
-function mouseWheel(event) {
-	bg.s -= event.delta / 30;
-	if (bg.s > 100) bg.s = 100;
-	if (bg.s < 0) bg.s = 0;
+
+function noiseVal() {
+	xoff += 0.01;
+	return noise(xoff);
 }
 
 function windowResized() {
