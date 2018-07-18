8b6218d294986aeeb41b8bc92c8d2c2568e1ee41
dvinubius <dvinubiu.narmon@gmail.com>
2018-07-18 16:51:43 +0300

    ignoring dist folder now

--- dist/libraries/utilities.js
+++
@@ -1,5 +0,0 @@
-function mouseOnEdge(tol) {
-	const tolerance = tol ? tol : 10;
-	return mouseX <= tolerance || mouseY <= tolerance ||
-				mouseX >= width - tolerance || mouseY >= height - tolerance;
-}
